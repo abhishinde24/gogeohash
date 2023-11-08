3356c4bc9d2985fbc0e3e581235dc7df041a2109
abhi24 <abhishekshinde2211@gmail.com>
2023-11-08 09:08:24 +0530

    Add: Decode functionality to deconde hash to lat,log

--- gogeohash.go
+++ gogeohash.go
@@ -1,13 +1,32 @@
 package gogeohash
 
+import (
+	"errors"
+	"math"
+	"strings"
+)
+
+type point struct {
+	lat float64
+	lon float64
+}
+type bounds struct {
+	sw point
+	ne point
+}
+
 const base32 = "0123456789bcdefghjkmnpqrstuvwxyz" // (geohash-specific) Base32 map
+
+func roundToPrecision(value float64, precision int) float64 {
+	multiplier := math.Pow(10, float64(precision))
+	return math.Round(value*multiplier) / multiplier
+}
 
 type GeoHash struct{}
 
 func (g GeoHash) Encode(lat float64, lon float64, precision int) (string, error) {
 	/* try to get precision by lat and log precision given
 	 */
-
 	if precision < 0 || precision > 12 {
 		precision = 12
 	}
@@ -54,4 +73,72 @@ func (g GeoHash) Encode(lat float64, lon float64, precision int) (string, error)
 		}
 	}
 	return geohash, nil
+}
+
+func (g GeoHash) Decode(geohash string) (float64, float64, error) {
+	bound,err := g.bound(geohash)
+	if err != nil {
+		return 360.0,360.0,err
+	}
+	latMin := bound.sw.lat
+	lonMin := bound.sw.lon
+	latMax := bound.ne.lat
+	lonMax := bound.ne.lon
+
+	lat := (latMax + latMin) / 2
+	lon := (lonMax + lonMin) / 2
+
+	latPrecision := int(2 - math.Log10(latMax-latMin))
+	lonPrecision := int(2 - math.Log10(lonMax-lonMin))
+
+	roundedLat := roundToPrecision(lat, latPrecision) 
+	roundedLon := roundToPrecision(lon, lonPrecision)
+
+	return roundedLat,roundedLon,nil
+}
+
+func (g GeoHash)bound(geohash string) (bounds,error){
+	if len(geohash) == 0 {
+		return bounds{}, errors.New("Invalid hash")
+	}
+	geohash = strings.ToLower(geohash)
+
+	evenBit := true
+	latMin := -90.0
+	latMax := 90.0
+	lonMin := -180.0
+	lonMax := 180.0
+
+	for i :=0 ; i < len(geohash); i++ {
+		chr := geohash[i]
+		idx := strings.Index(base32,string(chr)) 
+		if 	idx == -1 {
+			return bounds{}, errors.New("Invalid char geohash") 
+		}
+
+		for k := 4 ; k >= 0 ; k-- {
+			setBit := idx >> k & 1
+			if evenBit {
+				lonMid := (lonMax + lonMin) / 2
+				if setBit == 1 {
+					lonMin = lonMid	
+				} else {
+					lonMax = lonMid	
+				}
+			} else {
+				latMid := (latMax + latMin) / 2
+				if setBit == 1 {
+					latMin = latMid	
+				} else {
+					latMax = latMid	
+				}
+			}
+			evenBit = !evenBit
+		}
+	}
+	bound := bounds{
+		sw: point{latMin,lonMin},
+		ne: point{latMax,lonMax},
+	}
+	return bound,nil
 }
--- gogeohash_test.go
+++ gogeohash_test.go
@@ -5,15 +5,44 @@ import (
 	"testing"
 )
 
-func TestHash(t *testing.T) {
+func TestEncodeHash(t *testing.T) {
     g := GeoHash{}
 	lat1 := 48.8566
-	log1 := 2.35222
+	lon1 := 2.35222
 	prec := 8
 	exp_hash := "u09tvw0f"
-	hash, err := g.encode(lat1, log1, prec)
+	hash, err := g.Encode(lat1, lon1, prec)
 	if err != nil {fmt.Printf(hash);}
     if hash != exp_hash {
         t.Errorf("Expected 5, but got %s", hash)
     }
+}
+func TestDecodeHash(t *testing.T) {
+    g := GeoHash{}
+	lat1 := 48.85663
+	lon1 := 2.35228
+	geo_hash := "u09tvw0f"
+	lat,lon, err := g.Decode(geo_hash)
+	if err != nil {fmt.Printf("lat %g , log %g Error %s",lat,lon,err);}
+    if lat1 != lat && lon1 != lon {
+        t.Errorf("Expected %g & %g, but got %g & %g",lat1,lon1,lat,lon)
+    }
+}
+
+func BenchmarkEncodeHash(b *testing.B) {
+    g := GeoHash{}
+	lat1 := 48.8566
+	lon1 := 2.35222
+	prec := 8
+	for i :=0; i < b.N ; i++{
+		g.Encode(lat1, lon1, prec)
+	}
+}
+
+func BenchmarkDecodeHash(b *testing.B) {
+    g := GeoHash{}
+	geo_hash := "u09tvw0f"
+	for i :=0; i < b.N ; i++{
+		g.Decode(geo_hash)
+	}
 }
